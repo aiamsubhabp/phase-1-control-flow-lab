19c5ff7286c10dab68b8f529346777ec297ce433
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-02-25 19:36:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400) {
+    result = 'This one is on me!';
+    
+  }
+  else if (feet > 400 && feet <2000) {
+    result = 'That will be twenty bucks.';
+    
+  }
+  else if (feet > 2000 && feet < 2500){
+    result = 'I will gladly take your thirty bucks.';
+    
+  }
+  else {
+    result = 'No can do.';
+    
+  }
+  return result;
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+    break;
+
+}
 }
